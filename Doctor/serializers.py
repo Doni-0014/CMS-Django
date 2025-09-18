a80febaabadc59b10a4bdf80284f9a2594a706e3
Doni Alston <donialston46@gmail.com>
2025-09-18 19:51:41 +0530

    Serializers added for doctor

--- Doctor/serializers.py
+++ Doctor/serializers.py
@@ -2,14 +2,50 @@ from rest_framework import serializers
 from .models import Consultation, MedicinePrescription
 
 class MedicinePrescriptionSerializer(serializers.ModelSerializer):
-    model = MedicinePrescription
-    fields = ['prescription_id', 'consultation', 'medicine', 'dosage', 'frequency', 'duration_days', 'created_at', 'is_active']
-    read_only_fields = ['prescription_id', 'created_at'] 
+    class Meta:
+        model = MedicinePrescription
+        fields = ['prescription_id', 'consultation', 'medicine', 'dosage', 'frequency', 'duration_days', 'created_at', 'is_active']
+        read_only_fields = ['prescription_id', 'created_at'] 
+
+    def validate_dosage(self, value):
+        if not value:
+            raise serializers.ValidationError("Dosage cannot be empty!")
+        if len(value.strip())<5:
+            raise serializers.ValidationError("Dosage must be atleast 5 characters long!")
+        return value
+
+    def validate_frequency(self, value):
+        if not value:
+            raise serializers.ValidationError("Frequency cannot be empty!")
+        if len(value.strip())<5:
+            raise serializers.ValidationError("Frequency must be atleast 5 characters long!")
+        return value
+    
+    def validate_duration_days(self, value):
+        if value is not None and value <= 0:
+            raise serializers.ValidationError("Duration must be a positive number.")
+        return value
+
 
 class ConsultationSerializer(serializers.ModelSerializer):
     medicine_prescriptions = MedicinePrescriptionSerializer(many=True, read_only=True)
 
     class Meta:
         model = Consultation
-        fields = ['consultation_id', 'appointment', 'patient', 'doctor', 'symptoms', 'diagnosis', 'notes', 'created_at', 'is_active', 'medicine_prescriptions'],
+        fields = ['consultation_id', 'appointment', 'patient', 'doctor', 'symptoms', 'diagnosis', 'notes', 'created_at', 'is_active', 'medicine_prescriptions']
         read_only_fields = ['consultation_id', 'created_at', 'medicine_prescriptions']
+
+    def validate_symptoms(self, value):
+        if value and len(value.strip())<5:
+            raise serializers.ValidationError("Symptoms must be atleast 5 characters long!")
+        return value
+    
+    def validate_diagnosis(self, value):
+        if value and len(value.strip())<5:
+            raise serializers.ValidationError("Diagnosis must be atleast 5 characters long!")
+        return value
+    
+    def validate_notes(self, value):
+        if value and len(value.strip())<5:
+            raise serializers.ValidationError("Notes must be atleast 5 characters long!")
+        return value
