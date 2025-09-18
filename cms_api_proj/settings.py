7e4c1bf21e914bfe12312d26ae2f9169aa99a327
Doni Alston <donialston46@gmail.com>
2025-09-18 12:39:26 +0530

    Rest framework added in settings.py

--- cms_api_proj/settings.py
+++ cms_api_proj/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'Authentication',
     'Doctor',
     'Pharmacist',
-    'Receptionist'
+    'Receptionist',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+               'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+                'rest_framework.permissions.IsAuthenticated',
+    ),
+
+}
