6558719a080eca93ce242b7e4b158164e72d9281
Doni Alston <donialston46@gmail.com>
2025-09-18 14:26:48 +0530

    Doctor-Views added

--- Doctor/views.py
+++ Doctor/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, filters
+from .models import Consultation, MedicinePrescription
+from .serializers import ConsultationSerializer, MedicinePrescriptionSerializer
 # Create your views here.
+
+class ConsultationViewSet(viewsets.ModelViewSet):
+    queryset = Consultation.objects.all()
+    serializer_class = ConsultationSerializer
+    #permission_classes = [IsDoctor]
+    #Search 
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['consultation_id']
+
+class MedicinePrescriptionViewSet(viewsets.ModelViewSet):
+    queryset = MedicinePrescription.objects.all()
+    serializer_class = MedicinePrescriptionSerializer
+    # permission_classes = [IsDoctor]
+    #Search 
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['prescription_id']
