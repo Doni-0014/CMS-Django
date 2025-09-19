78d0668bc691c8826b0064ca54e5b5221a291e2f
Doni Alston <donialston46@gmail.com>
2025-09-19 12:22:09 +0530

    Consultation and MedPrescription ID Feature Added

--- Doctor/models.py
+++ Doctor/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 # Create your models here.
 
 class Consultation(models.Model):
-    consultation_id = models.AutoField(primary_key=True)
+    consultation_id = models.CharField(max_length=20, primary_key=True, editable=False)
     appointment = models.ForeignKey()
     patient = models.ForeignKey()
     doctor = models.ForeignKey()
@@ -13,17 +13,39 @@ class Consultation(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
 
+    def save(self, *args, **kwargs):
+        if not self.consultation_id:
+            last_id = Consultation.objects.all().order_by('created_at').last()
+            if last_id:
+                last_number = int(last_id.consultation_id.replace('CSTID', ''))
+                new_number = last_number + 1
+            else:
+                new_number = 1001
+            self.consultation_id = f"CSTID{new_number}"
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"Consultation #{self.id} - Patient (Patient ID) with Dr (Doctor ID)"
+        return f"Consultation #{self.consultation_id} - Patient (Patient ID) with Dr (Doctor ID)"
 
 class MedicinePrescription(models.Model):
-    prescription_id = models.AutoField(primary_key=True)
+    prescription_id = models.CharField(max_length=20, primary_key=True, editable=False)
     consultation = models.ForeignKey()
     medicine = models.ForeignKey()
     dosage = models.CharField(max_length=200)
     frequency = models.CharField(max_length=200)
     duration_days = models.PositiveIntegerField(null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        if not self.prescription_id:
+            last = MedicinePrescription.objects.all().order_by('created_at').last()
+            if last:
+                last_number = int(last.prescription_id.replace('MEDPR', ''))
+                new_number = last_number + 1
+            else:
+                new_number = 1001
+            self.prescription_id = f"MEDPR{new_number}"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"(Medicine) for consultation (self.consultation)"
