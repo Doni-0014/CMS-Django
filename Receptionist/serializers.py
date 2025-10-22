1b4f6123d84363c4077d596f3938c3ba864a94a9
Sushmitha-2004-sush <achusushmi44@gmail.com>
2025-10-22 14:29:33 +0530

    integration works

--- Receptionist/serializers.py
+++
@@ -1,9 +0,0 @@
-#duplicated file
-
-from rest_framework import serializers
-from .models import ReceptionistProfile
-
-class ReceptionistProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ReceptionistProfile
-        fields = ['id', 'user', 'phone']
