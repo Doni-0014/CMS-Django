f5742274974a676ba3c9aecaf54c43ceb3b8e08c
Doni Alston <donialston46@gmail.com>
2025-10-23 23:41:44 +0530

    Error rectified

--- Pharmacist/urls.py
+++ Pharmacist/urls.py
@@ -1,6 +1,12 @@
 # Pharmacist/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenBlacklistView,
+)
 from .views import (
     MedicineViewSet,
     MedicineCategoryViewSet,
